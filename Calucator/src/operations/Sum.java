d245922582ed8b31c0f6ece52fe632986d96ec40
CezG <cezarygulba@gmail.com>
2020-11-02 17:07:42 +0100

    updated class Sum to new class Length

--- Calucator/src/operations/Sum.java
+++ Calucator/src/operations/Sum.java
@@ -1,29 +1,26 @@
 package operations;
 
-import units.Unit;
-import units.ULength;
-//import units.UWeight;
+import units.EnumLength;
+import units.Length;
 
 public class Sum {
 
-	public double sum(ULength unitLength,Unit...table) {
+	public double sum(EnumLength unitLength, Length... table) {
 		double result = 0;
-		for(Unit x : table ) {
+		for (Length x : table) {
 			result += x.getData();
 		}
-		return  result/unitLength.converter ; 
+		return result / unitLength.converter;
 	}
 
-	
-//	public static void main(String[]args) {
-//		Unit u1 = new Unit(5,ULength.M);
-//		u1.showData();
-//		Unit u2 = new Unit(7,ULength.KM);
-//		u2.showData();
-//		Sum s= new Sum();
-//		double r=s.sum(ULength.KM,u1,u2);
-//		System.out.println(r);
-//	}
-	
-	
+	public static void main(String[] args) {
+		Length u1 = new Length(5, EnumLength.M);
+		u1.showData();
+		Length u2 = new Length(7, EnumLength.KM);
+		u2.showData();
+		Sum s = new Sum();
+		double r = s.sum(EnumLength.KM, u1, u2);
+		System.out.println(r);
+	}
+
 }
