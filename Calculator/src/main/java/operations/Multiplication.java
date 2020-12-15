627f6e2b6d755da955e6f9c485dcf40b1c2869b4
CezG <cezarygulba@gmail.com>
2020-12-16 00:44:06 +0100

    added multiplication

--- Calculator/src/main/java/operations/Multiplication.java
+++ Calculator/src/main/java/operations/Multiplication.java
@@ -1,5 +1,63 @@
 package operations;
 
+import units.EnumLength;
+import units.EnumWeight;
+import units.Length;
+import units.Weight;
+
 public class Multiplication {
+	
+	public double multiplication(EnumLength unit, Length... table) throws IllegalArgumentException {
+
+		double result = 1;
+		if (table.length != 0) {
+			for (Length x : table) {
+				result *= x.getData();
+			}
+			return result / unit.converter;
+
+		} else {
+			throw new IllegalArgumentException("There is no arguments");
+		}
+	}
+
+	public double multiplication(EnumWeight unit, Weight... table) throws IllegalArgumentException {
+
+		if (table.length != 0) {
+			double result = 1;
+			for (Weight x : table) {
+				result *= x.getData();
+			}
+			return result / unit.converter;
+		} else {
+			throw new IllegalArgumentException("There is no arguments");
+		}
+
+	}
+	
+//	public static void main(String[] args) {
+//	
+//	Length u1 = new Length(5, EnumLength.M);
+//	u1.showData();
+//	Length u2 = new Length(7, EnumLength.KM);
+//	u2.showData();
+//	Multiplication m = new Multiplication();
+//	double r1 = m.multiplication(EnumLength.KM, u1, u2);
+//	System.out.println(r1);
+//	
+//	
+//
+//	Weight u3 = new Weight(6, EnumWeight.KG);
+//	u3.showData();
+//	Weight u4 = new Weight(8, EnumWeight.T);
+//	u4.showData();
+//	Multiplication m2 = new Multiplication();
+//	double r2 = m2.multiplication(EnumWeight.KG, u3, u4);
+//	System.out.println(r2);
+//	
+//	m2.multiplication(EnumWeight.KG );
+//}
+
+
 
 }
